5671df9fb4b6d86d883e52a6cf51fd8f83066330
Milena Vitoria dos Santos Golnik <141414014+milenagolnik@users.noreply.github.com>
2024-11-07 08:38:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    "Física",
+    "Qual a fórmula que calcula velocidade média?",
+    "VM = ΔS/Δt"
+)
+
+criaCartao(
+    "Química",
+    "O que é um elemento químico?",
+    "Elemento químico é o conjunto de átomos de mesmo número atômico. O número atômico é a quantidade de prótons que um átomo possui em seu núcleo."
+)
+
+criaCartao(
+    "Matemática",
+    "Qual o valor de PI?",
+    " 3,141592653..., mas a aproximação mais utilizada para o valor de π é 3,14"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital do Brasil?",
+    "Brasília"
+)
+
+criaCartao(
+    "História",
+    "O que foi o regime militar?",
+    "É uma forma de governo autoritário onde o poder político é efetivamente controlado por militares"
+)
+
+criaCartao(
+    "Português",
+    "Quem escreveu o livro Diva-1864?",
+    "José de Alencar"
+)
